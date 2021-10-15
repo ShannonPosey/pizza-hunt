d6a69275b754d7923cfce228dba54bad342a56a2
shannon_p <snp1284kasey@aol.com>
2021-10-14 21:58:05 -0400

    added validation to the app

--- models/Pizza.js
+++ models/Pizza.js
@@ -4,10 +4,15 @@ const dateFormat = require("../utils/dateFormat");
 const PizzaSchema = new Schema (
     {
     pizzaName: {
-        type: String
+        type: String,
+        required: " You need to provide a pizza name!",
+        // required: true,
+        trim: true
     },
     createdBy: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     createdAt: {
         type: Date,
@@ -16,6 +21,9 @@ const PizzaSchema = new Schema (
     },
     size: {
         type: String,
+        required: true,
+        // enum - stands for enumerable (refers to a set of data that can be iterated over like the for loop)
+        enum: ["Personal", "Small", "Medium", "Large", "Extra Large"],
         default: "Large"
     },
     toppings: [],
