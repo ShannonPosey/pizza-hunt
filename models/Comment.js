ef64084eef9ebe0abc5ca0f7f561762eabebf917
shannon_p <snp1284kasey@aol.com>
2021-10-14 21:05:50 -0400

    added online capabilities

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,9 @@
 const { Schema, model, Types} = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
-const ReplySchema = new Schema (
+const ReplySchema = new Schema(
     {
-        // set custom id to avoid confusion with parent comment _id
+        // set custom id to avid confusion with parent comment_id
         replyId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId()
@@ -11,10 +11,10 @@ const ReplySchema = new Schema (
         replyBody: {
             type: String
         },
-        writtenBY: {
+        writtenBy: {
             type: String
         },
-        createdAt: {
+        createAt: {
             type: Date,
             default: Date.now,
             get: createdAtVal => dateFormat(createdAtVal)
@@ -27,35 +27,35 @@ const ReplySchema = new Schema (
     }
 );
 
-const CommentSchema = new Schema ({
-    writtenBy: {
-        type: String
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        // use ReplySchema to validate data for a reply
+        replies: [ReplySchema]
     },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: createdAtVal => dateFormat(createdAtVal)
-    },
-    // use ReplySchema to validate data for a reply
-    replies: [ReplySchema]
-},
-{
-    toJSON: {
-        virtuals: true,
-        getters: true
-    },
-    id: false
-}
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
 );
 
-// get total count of replies on retrieval
 CommentSchema.virtual("replyCount").get(function() {
     return this.replies.length;
 });
 
-const Comment = model ("Comment", CommentSchema);
+const Comment = model("Comment", CommentSchema);
 
 module.exports = Comment;
